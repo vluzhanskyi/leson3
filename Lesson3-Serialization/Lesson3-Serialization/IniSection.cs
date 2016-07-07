a9090e392159ebcf43ce951b85be611e58102107
vluzhanskyi <Vluzhanskyi@gmail.com>
2016-07-07 16:11:24 +0300

    test

---
+++ Lesson3-Serialization/Lesson3-Serialization/IniSection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3_Serialization
+{
+    class IniSection : Attribute
+    {
+        public IniSection(string elementName)
+        {
+            this.ElementName = elementName;
+        }
+
+        public string ElementName { get; set; }
+    }
+}
---
+++ Lesson3-Serialization/Lesson3-Serialization/iniKey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3_Serialization
+{
+    class iniKey : Attribute
+    {
+        public iniKey(string elementName)
+        {
+            this.ElementName = elementName;
+        }
+
+        public string ElementName { get; set; }
+    }
+}
